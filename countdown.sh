97e72813f320391c34efbe532f31f4a636016433
Liang Liu <nlprliu@gmail.com>
2015-05-19 21:32:25 -0400

    Refactored some code.

--- countdown.sh
+++ countdown.sh
@@ -32,27 +32,7 @@ _show_pop_up() {
    osascript -e 'display dialog "You need to take a break."' &>/dev/null
 }
 
-
-function countdown(){
-   tput civis # Hide cursor
-
-   # Do not echo input from stdin
-   if [ -t 0 ]; then stty -echo -icanon -icrnl time 0 min 0; fi
-   
-   # Handle ctrl-c and ctrl-z gracefully.
-   trap "_stop_countdown ; exit" SIGINT
-   trap "" SIGTSTP   
-
-   work_period=$1
-   break_period=$2
-
-   now=$(date +%s)
-   while true; do
-     echo "Started to work at"
-     date '+%H:%M:%S'
-     stop_date=$(($now + $work_period * 60)); 
-     _countdown_one_period $stop_date $stop_date
-
+_notify_user_when_display_is_on() {
      while true; do
          display_state=$(ioreg -r -d 1 -n IODisplayWrangler | grep -i IOPowerManagement | sed 's/.*DevicePowerState"=\([0-9]\).*/\1/g')	
          if [ $display_state -eq 4 ]; then 
@@ -70,20 +50,47 @@ function countdown(){
          stop_date=$(($now + 60));
          _countdown_one_period $stop_date $stop_date
      done
+}
 
-     start_date=$(($stop_date + $break_period * 60)); 
-     echo "Took a break at"
-     date -j -f '%s' $stop_date '+%H:%M:%S'
-     _countdown_one_period $stop_date $start_date
-     
-     # Do not start the next cycle when display is off.
-     while true; do
-         display_state=$(ioreg -r -d 1 -n IODisplayWrangler | grep -i IOPowerManagement | sed 's/.*DevicePowerState"=\([0-9]\).*/\1/g')	
-         if [ $display_state -eq 4 ]; then break; fi
-         sleep 10
+_wait_when_display_is_off (){
+    # Do not start the next cycle when display is off.
+    while true; do
+        display_state=$(ioreg -r -d 1 -n IODisplayWrangler | grep -i IOPowerManagement | sed 's/.*DevicePowerState"=\([0-9]\).*/\1/g')	
+        if [ $display_state -eq 4 ]; then break; fi
+        sleep 10
+    done
+    now=$(date +%s)
+}
+
+function countdown(){
+    tput civis # Hide cursor
+
+    # Do not echo input from stdin
+    if [ -t 0 ]; then stty -echo -icanon -icrnl time 0 min 0; fi
+   
+    # Handle ctrl-c and ctrl-z gracefully.
+    trap "_stop_countdown ; exit" SIGINT
+    trap "" SIGTSTP   
+
+    work_period=$1
+    break_period=$2
+
+    now=$(date +%s)
+    while true; do
+        echo "Started to work at"
+        date '+%H:%M:%S'
+        stop_date=$(($now + $work_period * 60)); 
+        _countdown_one_period $stop_date $stop_date
+
+        _notify_user_when_display_is_on
+
+        start_date=$(($stop_date + $break_period * 60)); 
+        echo "Took a break at"
+        date -j -f '%s' $stop_date '+%H:%M:%S'
+        _countdown_one_period $stop_date $start_date
+
+        _wait_when_display_is_off
      done
-     now=$(date +%s)
-   done
 }
 
 if [[ $# -eq 0 ]] ; then
