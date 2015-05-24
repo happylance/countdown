932572c434a1580c5ed328839dcededa2b94d685
happylance <nlprliu@gmail.com>
2015-05-24 12:55:41 -0400

    Update countdown.sh

--- countdown.sh
+++ countdown.sh
@@ -7,7 +7,7 @@ simple_time_format=0
 
 _echo_usage () {
     echo "usage: $0 [-s] [-u <1-60>] [-w <1-59>] [-b <1-10>]"
-    echo "-s Show countdown using simple time format which include minutes only."
+    echo "-s Show countdown using simple time format which includes minutes only."
     echo "-u Update period in seconds. Default is 1."
     echo "-w Work period in minutes. Default is 50."
     echo "-b Break period in minutes. Default is 10."
