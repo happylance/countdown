978c66efb8805ae116dd13b8b03fbd5b6d1def42
Liang Liu <nlprliu@gmail.com>
2015-05-31 11:33:54 -0400

    Supported resuming from .countdown file when countdown.sh is restarted.

--- countdown.sh
+++ countdown.sh
@@ -76,6 +76,7 @@ _countdown_one_period() {
     [ $log_to_file -eq 1 ] && echo -e "$period_type\n$one_period_stop_date" > "$file_name"
  
     keypress=''
+    now=$(date +%s)
     while [ "$one_period_stop_date" -gt "$now" ]; do
         _echo_countdown $one_period_stop_date $now
         count=0
@@ -104,6 +105,46 @@ _wait_when_display_is_off (){
     now=$(date +%s)
 }
 
+_countdown_work_period () {
+    work_stop_date=$1
+    echo -e "\033[0mStarted to work at" # Normal color
+    date '+%H:%M:%S'
+    _countdown_one_period "w" $work_stop_date
+
+    actual_work_stop_date=$(($previous_date + 1))
+    now=$(date +%s)
+    break_stop_date=$(($actual_work_stop_date + $break_period * 60)); 
+    [ $now -lt $break_stop_date ] && [ $allow_notification -eq 1 ] && _show_notification
+}
+
+_countdown_break_period () {
+    break_stop_date=$1
+    actual_work_stop_date=$2
+    echo -e "\033[0;32mTook a break at" # Green color
+    date -j -f '%s' $actual_work_stop_date '+%H:%M:%S'
+    _countdown_one_period "b" $break_stop_date
+    _wait_when_display_is_off
+}
+
+_resume_previous_countdown() {
+    [ -f $file_name ] || return
+    args=($(cat "$file_name"))  
+    period_type=${args[0]}
+    period_stop_date=${args[1]}
+
+    now=$(date +%s)
+    if [ "$period_stop_date" -gt "$now" ]; then
+        if [ $period_type == 'w' -a $period_stop_date -lt $(($now + $work_period * 60)) ]; then
+            _countdown_work_period $period_stop_date
+            _countdown_break_period $break_stop_date $actual_work_stop_date && return
+        fi
+
+        if [ $period_type == 'b' -a $period_stop_date -lt $(($now + $break_period * 60)) ]; then
+            _countdown_break_period $period_stop_date $now && return
+        fi
+    fi
+}
+
 function countdown(){
     tput civis # Hide cursor
 
@@ -117,23 +158,12 @@ function countdown(){
     work_period=$1
     break_period=$2
 
+    _resume_previous_countdown
     now=$(date +%s)
     while true; do
-        echo -e "\033[0mStarted to work at" # Normal color
-        date '+%H:%M:%S'
         work_stop_date=$(($now + $work_period * 60)); 
-        _countdown_one_period "w" $work_stop_date
-
-        actual_work_stop_date=$(($previous_date + 1))
-        now=$(date +%s)
-        break_stop_date=$(($actual_work_stop_date + $break_period * 60)); 
-        [ $now -lt $break_stop_date ] && [ $allow_notification -eq 1 ] && _show_notification
-
-        echo -e "\033[0;32mTook a break at" # Green color
-        date -j -f '%s' $actual_work_stop_date '+%H:%M:%S'
-        _countdown_one_period "b" $break_stop_date
-
-        _wait_when_display_is_off
+        _countdown_work_period $work_stop_date
+        _countdown_break_period $break_stop_date $actual_work_stop_date
      done
 }
 
