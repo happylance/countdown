9c9c10a38ebb2175c9d904f10157bc920e3d1f8a
Liang Liu <nlprliu@gmail.com>
2015-05-30 21:33:58 -0400

    Removed parameter requirement for remain_minutes.sh.

--- remain_minutes.sh
+++ remain_minutes.sh
@@ -7,14 +7,10 @@ read_countdown() {
     period_type=${args[0]}
     period_stop_date=${args[1]}
 
-    if ! [[ 'x'$period_type == 'x'$1 ]]; then
-        return;
-    fi
-
     now=$(date +%s)
     if [ "$period_stop_date" -gt "$now" ]; then
-    	echo "$(date -j -f '%s' $(($period_stop_date - $now )) '+%M')\r";
+    	echo "$period_type""$(date -j -f '%s' $(($period_stop_date - $now )) '+%M')\r";
     fi
 }
 
-read_countdown $1 
+read_countdown 
